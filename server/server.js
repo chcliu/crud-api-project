eaa5b1ab5c24f3e5beb5de475453c4760ea09e41
chcliu <liucharlesche@gmail.com>
2018-10-27 14:51:21 +0900

    Was able to get a response from the server.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const apiRouter = require("./api.router.js");
+app.use(
+    "/", // add additional middleware to the server, mounted on the /api/ path
+    express.json(), // enables parsing of application/json request bodies
+    express.urlencoded({ extended: true }), // enables parsing of application/x-www-form-urlencoded data
+    apiRouter
+);
+
 let port = 4000;
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
